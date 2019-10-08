0b6035bd465700411e695bca5d515e53856eb6f5
obara-k <obara.k@b-style.net>
2019-10-08 20:55:20 +0900

    # tenki form

---
+++ components/TenkiCard.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="column">
+    <div class="card">
+      <header class="card-header">
+        <p class="card-header-title has-text-grey">
+          {{ name }}
+        </p>
+        <div class="content has-text-centered">
+
+        </div>
+      </header>
+      <div class="card-content">
+        <div class="card-content-inner">
+          <div>{{ icon }}</div>
+            <ul>
+              <li>{{ latestDate }}</li>
+              <li>{{ latestTenki }}</li>
+          </ul>
+        </div>
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+    icon: {
+      type: String,
+      required: true
+    },
+    latestDate: {
+      type: String,
+      required: true
+    },
+    latestTenki: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
--- pages/fusen.vue
+++ pages/fusen.vue
@@ -1,69 +1,103 @@
 <template>
   <section class="section">
     <div class="input_wrapper">
-      <b-field label="Subject">
+      <b-field label="Name">
         <b-input
-          v-model="form.title"
-          placeholder="title"
+          v-model="inputs.name"
+          placeholder="name"
         ></b-input>
       </b-field>
-      <b-field label="Description">
+      <b-field label="icon">
         <b-input
-          v-model="form.description"
-          maxlength="4000"
-          type="textarea"
-          placeholder="description"
+          v-model="inputs.icon"
+          placeholder="icon"
         ></b-input>
+      </b-field>
+      <b-field label="Date">
+        <b-datepicker
+          :show-week-number="showWeekNumber"
+          placeholder="Click to select..."
+          v-model="inputs.latestDate"
+          icon="calendar-today">
+        </b-datepicker>
+      </b-field>
+      <b-field label="Status">
+        <b-select v-model="inputs.latestTenki">
+          <option
+            v-for="cond in status"
+            :key="cond.key"
+            :value="cond.key"
+          >
+            {{ cond.key }}
+          </option>
+        </b-select>
       </b-field>
       <b-button
-        @click="addCard"
+        @click="addTenkiCard"
         type="is-info"
       >
         Submit
       </b-button>
     </div>
     <div class="cardWrap">
-      <!--
-      <card
-        title="Free"
-        icon="github-circle"
-      >
-        Open source on <a href="https://github.com/buefy/buefy">
-          GitHub
-        </a>
-      </card>
-    -->
-      <card
+      <TenkiCard
         class="fusen_card"
-        v-for="(item, index) in items"
+        v-for="(member, index) in members"
         :key="index"
-        :title="item.title"
+        :name="member.name"
+        :icon="member.icon"
+        :latestDate="member.latestDate"
+        :latestTenki="member.latestTenki"
       >
-        {{ item.description }}
         <b-button
-          @click="removeCard(index)"
+          @click="removeTenkiCard(index)"
           type="is-light"
           icon-right="delete"
         />
-      </card>
+      </TenkiCard>
     </div>
   </section>
 </template>
 
 <script>
 import Card from '~/components/Card'
+import TenkiCard from '~/components/TenkiCard'
 
 export default {
-  name: 'HomePage',
+  name: 'fusen',
 
   components: {
-    Card
+    Card,
+    TenkiCard
   },
   data() {
     return {
+      showWeekNumber: false,
       items: [
         { title: 'Free', description: 'test project'}
       ],
+      members: [
+        { name: 'Kunito Obara', icon: 'KO', latestDate:'2019/10/08', latestTenki: 'Normal' , log: [
+            { date: '2019/10/07' , tenki: 'Good' },
+            { date: '2019/10/06' , tenki: 'VeryGood' },
+            { date: '2019/10/05' , tenki: 'Bad' },
+            { date: '2019/10/04' , tenki: 'VeryBad' }
+          ]
+        }
+      ],
+      status: [
+        { key: 'VeryGood' },
+        { key: 'Good' },
+        { key: 'Normal' },
+        { key: 'Bad' },
+        { key: 'VeryBad' }
+      ],
+      inputs: {
+        name: '',
+        latestDate: '',
+        latestTenki: '',
+        icon: ''
+      },
       form: {
         title: '',
         description: '',
@@ -81,10 +115,21 @@ export default {
       this.form.title = ''
       this.form.description = ''
     },
+    addTenkiCard() {
+      let temp = []
+      const data = JSON.parse(JSON.stringify(this.members))
+      const model = JSON.parse(JSON.stringify(this.inputs))
+      temp = data
+      temp.push(model)
+      this.members = temp
+      this.inputs = { name: '', date: '', status: '', icon: ''}
+    },
     removeCard(key) {
       this.items.splice(key,1)
+    },
+    removeTenkiCard(key) {
+      this.members.splice(key,1)
     }
-
   }
 }
 </script>
@@ -94,8 +139,7 @@ export default {
   flex-wrap: wrap;
   justify-content: left;
   .fusen_card {
-    flex-grow: 0;
-    flex-basis: 32%;
+    flex-grow: 3;
   }
 }
 
